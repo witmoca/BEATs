5146df529b8a6716e94e97377064202d1414107f
Jente Heremans <jente@witmoca.be>
2018-12-13 13:33:13 +0100

    Added Playlist song add/edit capability (empty row) see #10

--- src/main/java/be/witmoca/BEATs/model/PlaylistTableModel.java
+++ src/main/java/be/witmoca/BEATs/model/PlaylistTableModel.java
@@ -35,13 +35,13 @@ import be.witmoca.BEATs.Launch;
 public class PlaylistTableModel extends AbstractTableModel implements DataChangedListener {
 	private static final long serialVersionUID = 1L;
 	private String PlaylistName;
-	private static final String COLUMN_NAME[] = {"Artist","Song","Comment"};
+	private static final String COLUMN_NAME[] = { "Artist", "Song", "Comment" };
 	private List<PlaylistEntry> playlistList = null;
 
 	public PlaylistTableModel(String playlistName) {
 		super();
 		this.setPlaylistName(playlistName);
-		
+
 		Launch.getDB_CONN().addDataChangedListener(this, DataChangedListener.DataType.PLAYLIST_DATA_OPTS);
 		tableChanged();
 	}
@@ -49,11 +49,12 @@ public class PlaylistTableModel extends AbstractTableModel implements DataChange
 	@Override
 	public void tableChanged() {
 		playlistList = new ArrayList<PlaylistEntry>();
-		try (PreparedStatement getValue = Launch.getDB_CONN().prepareStatement("SELECT Artist, Song, Comment FROM SongsInPlaylist WHERE PlaylistName = ?")) {
+		try (PreparedStatement getValue = Launch.getDB_CONN()
+				.prepareStatement("SELECT Artist, Song, Comment FROM SongsInPlaylist WHERE PlaylistName = ?")) {
 			getValue.setString(1, PlaylistName);
 			ResultSet value = getValue.executeQuery();
-			while(value.next()) {
-				playlistList.add(new PlaylistEntry(value.getString(1), value.getString(2) ,value.getString(3)));
+			while (value.next()) {
+				playlistList.add(new PlaylistEntry(value.getString(1), value.getString(2), value.getString(3)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -62,8 +63,8 @@ public class PlaylistTableModel extends AbstractTableModel implements DataChange
 	}
 
 	@Override
-	public int getRowCount() {		
-		return playlistList.size();
+	public int getRowCount() {
+		return playlistList.size() + 1;
 	}
 
 	@Override
@@ -73,7 +74,11 @@ public class PlaylistTableModel extends AbstractTableModel implements DataChange
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		return playlistList.get(rowIndex).getColumn(columnIndex);
+		try {
+			return playlistList.get(rowIndex).getColumn(columnIndex);
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 
 	@Override
@@ -81,9 +86,63 @@ public class PlaylistTableModel extends AbstractTableModel implements DataChange
 		return COLUMN_NAME[column];
 	}
 
-	
 	public void setPlaylistName(String playlistName) {
 		PlaylistName = playlistName;
 		tableChanged();
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return true;
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		if(rowIndex >= playlistList.size()) {
+			// INSERT NEW
+			if(((String) aValue).trim().isEmpty() )
+				return;
+			
+			String ins[] = {"","",""};
+			ins[columnIndex] = (String) aValue;
+			try {
+				SQLObjectTransformer.addSongInPlaylist(PlaylistName, ins[0], ins[1], ins[2]);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else {
+			
+			// Load with current values and update with new one (in array)
+			String values[] = new String[3];
+			for(int i = 0; i < values.length ; i++) {
+				values[i] = playlistList.get(rowIndex).getColumn(1);
+			}
+			values[columnIndex] = (String) aValue;
+			
+			try {
+				// check if a delete isn't more appropriate (all empty cells)
+				if(String.join("", values).trim().isEmpty()) {
+					try (PreparedStatement updateVal = Launch.getDB_CONN().prepareStatement("DELETE FROM SongsInPlaylist WHERE PlaylistName = ? AND Artist = ? AND Song = ? AND Comment = ?")) {
+						for(int i = 0; i < values.length; i++) {
+							updateVal.setString(2 + i, playlistList.get(rowIndex).getColumn(i)); //old values
+						}
+						updateVal.setString(1, PlaylistName);
+						updateVal.executeUpdate();
+					}
+				} else {
+					try (PreparedStatement updateVal = Launch.getDB_CONN().prepareStatement("UPDATE SongsInPlaylist SET Artist = ?, Song = ?, Comment = ? WHERE PlaylistName = ? AND Artist = ? AND Song = ? AND Comment = ?")) {
+						for(int i = 0; i < values.length; i++) {
+							updateVal.setString(1 + i, values[i]); // new values
+							updateVal.setString(5 + i, playlistList.get(rowIndex).getColumn(i)); //old values
+						}
+						updateVal.setString(4, PlaylistName);
+						updateVal.executeUpdate();
+					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		this.tableChanged();
 	}
 }
