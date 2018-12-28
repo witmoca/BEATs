witmoca/BEATs

commit 1e8b69d456857fa4df07e496c470a83d5478dda1

Rework of CCP/Drag-and-drop  * Removed drag and drop support  * Created custom clipboard class (backed by the CCP table)  * Created cut/cop/paste actions (closes #65 and closes #66)  * closes #30  * fixes bug #33

src/main/java/be/witmoca/BEATs/clipboard/TransferableSong.java
@@ -1,3 +1,13 @@
+/**
+ * 
+ */
+package be.witmoca.BEATs.clipboard;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
 /*
 *
 +===============================================================================+
@@ -17,46 +27,59 @@
 |    limitations under the License.                                             |
 +===============================================================================+
 *
-* File: PlaylistEntry.java
+* File: TransferableSong.java
 * Created: 2018
 */
-package be.witmoca.BEATs.model;
-
-class PlaylistEntry {
+public class TransferableSong implements Transferable {
+	public static final DataFlavor FLAVOR = new DataFlavor(TransferableSong.class, "Transferable Song");
+	
 	private final String ARTIST;
 	private final String SONG;
-	private final String COMMENT;	
 	private final int ROWID;
 	
-	PlaylistEntry(int rowid, String aRTIST, String sONG, String cOMMENT) {
-		super();
+	public TransferableSong(String aRTIST, String sONG, int rOWID) {
 		ARTIST = aRTIST;
 		SONG = sONG;
-		COMMENT = cOMMENT;
-		ROWID = rowid;
-	}
-	
-	
-	String getColumn(int i) {
-		switch(i) {
-		case 0: return this.ARTIST;
-		case 1: return this.SONG;
-		case 2: return this.COMMENT;
-		default: return null;
-		}
+		ROWID = rOWID;
 	}
 
-	@Override
-	public String toString() {
-		return ARTIST + " - " + SONG + (COMMENT.isEmpty() ? "" : " (" +  COMMENT + ")");
-	}
-	
-	public boolean isEmpty() {
-		return ARTIST.isEmpty() && SONG.isEmpty();
+	public String getARTIST() {
+		return ARTIST;
 	}
 
+	public String getSONG() {
+		return SONG;
+	}
 
 	public int getROWID() {
 		return ROWID;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.datatransfer.Transferable#getTransferDataFlavors()
+	 */
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		DataFlavor[] df = new DataFlavor[1];
+		df[0] = FLAVOR;
+		return df;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.datatransfer.Transferable#isDataFlavorSupported(java.awt.datatransfer.DataFlavor)
+	 */
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		return flavor.equals(FLAVOR);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.datatransfer.Transferable#getTransferData(java.awt.datatransfer.DataFlavor)
+	 */
+	@Override
+	public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+		if(!this.isDataFlavorSupported(flavor))
+			return null;		
+		return this;
 	}
 }

src/main/java/be/witmoca/BEATs/ApplicationManager.java
@@ -35,6 +35,7 @@ import javax.swing.SwingUtilities;
 
 import be.witmoca.BEATs.FileFilters.BEATsFileFilter;
 import be.witmoca.BEATs.actions.ExitApplicationAction;
+import be.witmoca.BEATs.clipboard.BEATsClipboard;
 import be.witmoca.BEATs.connection.ConnectionException;
 import be.witmoca.BEATs.connection.SQLConnection;
 import be.witmoca.BEATs.ui.ApplicationWindow;
@@ -45,6 +46,7 @@ public class ApplicationManager {
 
 	private static ApplicationWindow APP_WINDOW = null;
 	private static SQLConnection DB_CONN = null;
+	private static final BEATsClipboard INT_CLIP = new BEATsClipboard("BEATs Internal Clipboard");
 
 	// Start up
 	public static void main(String[] args) {		
@@ -172,5 +174,9 @@ public class ApplicationManager {
 
 	public static SQLConnection getDB_CONN() {
 		return DB_CONN;
+	}
+
+	public static BEATsClipboard getINT_CLIP() {
+		return INT_CLIP;
 	}
 }

src/main/java/be/witmoca/BEATs/clipboard/BEATsClipboard.java
@@ -1,15 +1,18 @@
 /**
  * 
  */
-package be.witmoca.BEATs.model;
+package be.witmoca.BEATs.clipboard;
 
-import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.io.IOException;
-import java.util.List;
 
-/*
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
+
+/**
 *
 +===============================================================================+
 |    BEATs (Burning Ember Archival Tool suite)                                  |
@@ -28,50 +31,34 @@ import java.util.List;
 |    limitations under the License.                                             |
 +===============================================================================+
 *
-* File: TransferableSong.java
+* File: BEATsClipboard.java
 * Created: 2018
 */
-public class TransferableSongs implements Transferable {
-	public static final String MIME_TYPE = "application/x.transferable-song-list";
+public class BEATsClipboard extends Clipboard {
+	private static final ClipboardTransferHandler handler = new ClipboardTransferHandler();
+	private static final JComponent COMPONENT_REPRESENTATION = new JLabel("Internal Clipboard");
 	
-	private List<PlaylistEntry> data;
-	
-	public TransferableSongs(List<PlaylistEntry> entries) {
-		super();
-		data = entries;
+	public BEATsClipboard(String name) {
+		super(name);
 	}
 
-
-	/* (non-Javadoc)
-	 * @see java.awt.datatransfer.Transferable#getTransferDataFlavors()
-	 */
 	@Override
-	public DataFlavor[] getTransferDataFlavors() {
-		DataFlavor[] df = new DataFlavor[1];
-		try {
-			df[0] = new DataFlavor(MIME_TYPE);
-		} catch (ClassNotFoundException e) {
-			return null;
-		}
-		return df;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.datatransfer.Transferable#isDataFlavorSupported(java.awt.datatransfer.DataFlavor)
-	 */
-	@Override
-	public boolean isDataFlavorSupported(DataFlavor flavor) {
-		return flavor.isMimeTypeEqual(MIME_TYPE);
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.datatransfer.Transferable#getTransferData(java.awt.datatransfer.DataFlavor)
-	 */
-	@Override
-	public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
-		if(!this.isDataFlavorSupported(flavor))
-			throw new UnsupportedFlavorException(flavor);
+	public synchronized void setContents(Transferable contents, ClipboardOwner owner) {
+		if(!(contents.isDataFlavorSupported(TransferableSong.FLAVOR)))
+			throw new IllegalArgumentException("Dataflavor is not acceptable");	
 		
-		return data;
+		if (owner != null)
+			owner.lostOwnership(this, contents);
+		
+		handler.importData(new TransferSupport(COMPONENT_REPRESENTATION, contents));
+	}
+
+	@Override
+	public synchronized Transferable getContents(Object requestor) {
+		return handler.createTransferable(COMPONENT_REPRESENTATION);
+	}	
+	
+	synchronized void pasteDone(Transferable t) {
+		handler.exportDone(COMPONENT_REPRESENTATION, t, TransferHandler.MOVE);
 	}
 }

src/main/java/be/witmoca/BEATs/clipboard/ClipboardActionFactory.java
@@ -1,0 +1,112 @@
+/**
+ * 
+ */
+package be.witmoca.BEATs.clipboard;
+
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
+import be.witmoca.BEATs.ApplicationManager;
+import be.witmoca.BEATs.ui.UiIcon;
+
+/*
+*
++===============================================================================+
+|    BEATs (Burning Ember Archival Tool suite)                                  |
+|    Copyright 2018 Jente Heremans                                              |
+|                                                                               |
+|    Licensed under the Apache License, Version 2.0 (the "License");            |
+|    you may not use this file except in compliance with the License.           |
+|    You may obtain a copy of the License at                                    |
+|                                                                               |
+|    http://www.apache.org/licenses/LICENSE-2.0                                 |
+|                                                                               |
+|    Unless required by applicable law or agreed to in writing, software        |
+|    distributed under the License is distributed on an "AS IS" BASIS,          |
+|    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   |
+|    See the License for the specific language governing permissions and        |
+|    limitations under the License.                                             |
++===============================================================================+
+*
+* File: ActionFactory.java
+* Created: 2018
+*/
+public class ClipboardActionFactory {
+	public static Action getCutAction(JComponent source) {
+		return new CutCopyAction(source, true);
+	}
+
+	public static Action getCopyAction(JComponent source) {
+		return new CutCopyAction(source, false);
+	}
+	
+	public static Action getPasteAction(JComponent source) {
+		return new PasteAction(source);
+	}
+
+	private static final class CutCopyAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		private final int action;
+		private final JComponent source;
+
+		private CutCopyAction(JComponent source, boolean cutCopy) {
+			super(cutCopy ? "Cut" : "Copy");
+			this.putValue(Action.SMALL_ICON, cutCopy ? UiIcon.CUT.getIcon() : UiIcon.COPY.getIcon());
+			action = cutCopy ? TransferHandler.MOVE : TransferHandler.COPY;
+			this.source = source;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			TransferHandler th = source.getTransferHandler();
+			Clipboard clipboard = ApplicationManager.getINT_CLIP();
+			if ((clipboard == null) || (th == null)) {
+				return;
+			}
+
+			try {
+				th.exportToClipboard(source, clipboard, action);
+			} catch (IllegalStateException ise) {
+				// Unavailable clipboard
+				return;
+			}
+		}
+	}
+
+	private static final class PasteAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		private final JComponent source;
+		
+		private PasteAction(JComponent source) {
+			super("Paste");
+			this.putValue(Action.SMALL_ICON, UiIcon.PASTE.getIcon());
+			this.source = source;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			TransferHandler th = source.getTransferHandler();
+			BEATsClipboard clipboard = ApplicationManager.getINT_CLIP();
+
+			if ((clipboard == null) || (th == null)) {
+				return;
+			}
+
+			try {
+				Transferable tr = clipboard.getContents(null);
+				if (tr != null) {
+					th.importData(new TransferSupport(source, tr));
+					clipboard.pasteDone(tr);
+				}
+			} catch (IllegalStateException ise) {
+				// Unavailable clipboard
+				return;
+			}
+		}
+	}
+}

src/main/java/be/witmoca/BEATs/clipboard/ClipboardTransferHandler.java
@@ -1,26 +1,21 @@
 /**
  * 
  */
-package be.witmoca.BEATs.ui.southpanel;
+package be.witmoca.BEATs.clipboard;
 
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.List;
-
 import javax.swing.JComponent;
-import javax.swing.JList;
 import javax.swing.TransferHandler;
 
 import be.witmoca.BEATs.ApplicationManager;
 import be.witmoca.BEATs.model.DataChangedListener;
-import be.witmoca.BEATs.model.PlaylistEntry;
-import be.witmoca.BEATs.model.TransferableSongs;
 
 /*
 *
@@ -44,15 +39,16 @@ import be.witmoca.BEATs.model.TransferableSongs;
 * File: CCPTransferHandler.java
 * Created: 2018
 */
-class CCPTransferHandler extends TransferHandler {
+public class ClipboardTransferHandler extends TransferHandler {
 	private static final long serialVersionUID = 1L;
+	private static int selected = -1;
 
 	@Override
 	public boolean canImport(TransferSupport support) {
 		DataFlavor[] transferFlavors = support.getDataFlavors();
 
 		for (DataFlavor df : transferFlavors) {
-			if (df.isMimeTypeEqual(TransferableSongs.MIME_TYPE))
+			if (df.equals(TransferableSong.FLAVOR))
 				return true;
 		}
 		return false;
@@ -69,25 +65,21 @@ class CCPTransferHandler extends TransferHandler {
 			return false;
 
 		try {
-			Object o = support.getTransferable().getTransferData(new DataFlavor(TransferableSongs.MIME_TYPE));
-			if (!(o instanceof List<?>)) {
-				throw new ClassCastException("Cannot cast " + o.getClass() + " to List<?>");
+			Object o = support.getTransferable().getTransferData(TransferableSong.FLAVOR);
+			if (!(o instanceof TransferableSong)) {
+				throw new ClassCastException("TransferableSong");
 			}
-			List<?> lpe = (List<?>) o;
-			try (PreparedStatement insertCCP = ApplicationManager.getDB_CONN().prepareStatement("INSERT INTO ccp (Artist, Song, Comment) VALUES (?,?,?)")) {
-				for (Object songO : lpe) {
-					PlaylistEntry pe = (PlaylistEntry) songO;
-					for (int i = 0; i < 3; i++)
-						insertCCP.setString(i + 1, pe.getColumn(i));
-					insertCCP.executeUpdate();
-				}
+			TransferableSong ts = (TransferableSong) o;
+			try (PreparedStatement insertCCP = ApplicationManager.getDB_CONN().prepareStatement("INSERT INTO ccp (Artist, Song) VALUES (?,?)")) {
+				insertCCP.setString(1, ts.getARTIST());
+				insertCCP.setString(2, ts.getSONG());
+				insertCCP.executeUpdate();
 			}
 			ApplicationManager.getDB_CONN().commit(EnumSet.of(DataChangedListener.DataType.CCP));
-		} catch (ClassNotFoundException | UnsupportedFlavorException | IOException | SQLException e) {
+		} catch (UnsupportedFlavorException | IOException | SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-
 		return true;
 	}
 
@@ -103,48 +95,48 @@ class CCPTransferHandler extends TransferHandler {
 
 	@Override
 	protected Transferable createTransferable(JComponent c) {
-		if (!(c instanceof JList))
+		if(selected < 0)
 			return null;
-
-		JList<?> list = ((JList<?>) c);
-		if (!(list.getModel() instanceof CCPListModel))
+		try (PreparedStatement selRow = ApplicationManager.getDB_CONN().prepareStatement("SELECT rowid, artist, song FROM CCP ORDER BY rowid ASC")) {
+			ResultSet rs = selRow.executeQuery();
+			for(int i = 0; i <= selected; i++) {
+				if(!rs.next())
+					return null;
+			}
+			return new TransferableSong(rs.getString(2), rs.getString(3), rs.getInt(1));
+		} catch (SQLException e) {
+			e.printStackTrace();
 			return null;
-		CCPListModel model = (CCPListModel) list.getModel();
-
-		int indices[] = list.getSelectedIndices();
-		List<PlaylistEntry> peList = new ArrayList<PlaylistEntry>();
-		for (int i = 0; i < indices.length; i++) {
-			peList.add(model.getEntry(indices[i]));
 		}
-		
-		return new TransferableSongs(peList);
 	}
 
 	@Override
 	protected void exportDone(JComponent source, Transferable data, int action) {
+		System.out.println(source);
+		System.out.println(data);
+		System.out.println(action);
 		if (action != TransferHandler.MOVE)
 			return;
 
 		try {
-			Object o = data.getTransferData(new DataFlavor(TransferableSongs.MIME_TYPE));
-			if (!(o instanceof List<?>)) {
-				throw new ClassCastException("Cannot cast " + o.getClass() + " to List<?>");
+			Object o = data.getTransferData(TransferableSong.FLAVOR);
+			if (!(o instanceof TransferableSong)) {
+				throw new ClassCastException("TransferableSong");
 			}
-			List<?> lpe = (List<?>) o;
+			TransferableSong ts = (TransferableSong) o;
 
-			try (PreparedStatement delRow = ApplicationManager.getDB_CONN().prepareStatement("DELETE FROM CCP WHERE Artist = ? AND Song = ? AND Comment = ?")) {
-				for (Object songO : lpe) {
-					PlaylistEntry pe = (PlaylistEntry) songO;
-					delRow.setString(1, pe.getColumn(0));
-					delRow.setString(2, pe.getColumn(1));
-					delRow.setString(3, pe.getColumn(2));
-					delRow.executeUpdate();
-				}
+			try (PreparedStatement delRow = ApplicationManager.getDB_CONN().prepareStatement("DELETE FROM CCP WHERE rowid = ?")) {
+				delRow.setInt(1, ts.getROWID());
+				delRow.executeUpdate();
 			}
 
 			ApplicationManager.getDB_CONN().commit(EnumSet.of(DataChangedListener.DataType.CCP));
-		} catch (SQLException | ClassNotFoundException | UnsupportedFlavorException | IOException e) {
+		} catch (SQLException | UnsupportedFlavorException | IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void setSelected(int selected) {
+		ClipboardTransferHandler.selected = selected;
 	}
 }

src/main/java/be/witmoca/BEATs/connection/SQLConnection.java
@@ -156,7 +156,7 @@ public class SQLConnection implements AutoCloseable {
 					"CREATE TABLE IF NOT EXISTS Episode(EpisodeId INTEGER PRIMARY KEY, EpisodeDate INTEGER NOT NULL UNIQUE)");
 			createEmptyTables.executeUpdate("CREATE TABLE IF NOT EXISTS Section(SectionName TEXT PRIMARY KEY)");
 			createEmptyTables.executeUpdate(
-					"CREATE TABLE IF NOT EXISTS ccp(rowid INTEGER PRIMARY KEY, Artist TEXT NOT NULL, Song TEXT NOT NULL, Comment TEXT)");
+					"CREATE TABLE IF NOT EXISTS ccp(rowid INTEGER PRIMARY KEY, Artist TEXT NOT NULL, Song TEXT NOT NULL)");
 			// Relation Tables
 			createEmptyTables.executeUpdate(
 					"CREATE TABLE IF NOT EXISTS SongsInPlaylist(rowid INTEGER PRIMARY KEY, PlaylistName REFERENCES Playlist NOT NULL,Artist TEXT NOT NULL, Song TEXT NOT NULL, Comment TEXT)");

src/main/java/be/witmoca/BEATs/ui/archivepanel/ArchivePopupMenu.java
@@ -7,6 +7,8 @@ import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JTable;
 
+import be.witmoca.BEATs.clipboard.ClipboardActionFactory;
+
 /**
 *
 +===============================================================================+
@@ -29,7 +31,7 @@ import javax.swing.JTable;
 * File: ArchivePopupMenu.java
 * Created: 2018
 */
-public class ArchivePopupMenu extends JPopupMenu {
+class ArchivePopupMenu extends JPopupMenu {
 	private static final long serialVersionUID = 1L;
 
 	protected ArchivePopupMenu(JTable table) {
@@ -38,6 +40,7 @@ public class ArchivePopupMenu extends JPopupMenu {
 		add(new JMenuItem(new RenameArtistAction(table)));
 		add(new JMenuItem(new RenameSongAction(table)));
 		add(new JMenuItem(new ChangeLocalAction(table)));
+		add(new JMenuItem(ClipboardActionFactory.getCopyAction(table)));
 		add(new JMenuItem(new DeleteEntryAction(table)));
 	}
 }

src/main/java/be/witmoca/BEATs/ui/archivepanel/ArchiveTable.java
@@ -23,30 +23,18 @@
 package be.witmoca.BEATs.ui.archivepanel;
 
 import java.awt.Component;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableModel;
-
+import be.witmoca.BEATs.clipboard.TransferableSong;
 import be.witmoca.BEATs.model.ArchiveTableModel;
-import be.witmoca.BEATs.model.PlaylistEntry;
-import be.witmoca.BEATs.model.TransferableSongs;
 import be.witmoca.BEATs.ui.extendables.SongTable;
 import be.witmoca.BEATs.ui.t4j.MultisortTableHeaderCellRenderer;
-import be.witmoca.BEATs.utils.UiUtils;
 
 class ArchiveTable extends SongTable {
 	private static final long serialVersionUID = 1L;
 
 	public ArchiveTable() {
 		super(new ArchiveTableModel());
-		this.getTableHeader().setReorderingAllowed(false);
-		
-		// Only a single line is allowed (some tools depend on this being true)
-		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		// Set custom renderer for the episode column
 		this.getColumnModel().getColumn(2).setCellRenderer(new EpisodeRenderer());
@@ -57,21 +45,23 @@ class ArchiveTable extends SongTable {
 		
 		this.setComponentPopupMenu(new ArchivePopupMenu(this));
 		
-		// Drag n drop logic
-		this.setDragEnabled(true);
+		// Drag and drop logic (no drag and drop, just Cut/Cop/Paste)
 		this.setTransferHandler(new ArchiveTransferHandler());
 	}
 
 	@Override
-	public TransferableSongs getSelectedSongs() {
-		int[] rowIndices = UiUtils.convertSelectionToModel(this.getSelectedRows(), this);
-		TableModel model = this.getModel();
+	public TransferableSong getSelectedSong() {
+		int rowIndex = this.getSelectedRow();
+		if(rowIndex < 0)
+			return null;
+		if(this.getRowSorter() != null)
+			rowIndex = this.getRowSorter().convertRowIndexToModel(rowIndex);
 		
-		List<PlaylistEntry> tfs = new ArrayList<PlaylistEntry>();		
-		for(int i = 0; i < rowIndices.length; i++) {
-			tfs.add(new PlaylistEntry(0, (String) model.getValueAt(rowIndices[i], 0), (String) model.getValueAt(rowIndices[i], 1), "" ) );
-		}
-		return new TransferableSongs(tfs);
+		if(!(this.getModel() instanceof ArchiveTableModel))
+			return null;
+		ArchiveTableModel model = (ArchiveTableModel) this.getModel();
+
+		return new TransferableSong((String) model.getValueAt(rowIndex, 0),(String) model.getValueAt(rowIndex, 1), model.getRowId(rowIndex));
 	}
 	
 	private static class EpisodeRenderer extends DefaultTableCellRenderer {

commit 899078b60f22918b71f65988eefeb5a5a5b47b04

Fixes #72 (display bug for CCPList) introduced in 1e8b69d456857fa4df07e496c470a83d5478dda1#diff-9f221e5c1369fa6271a9352493d9c880

src/main/java/be/witmoca/BEATs/clipboard/TransferableSong.java
@@ -82,4 +82,9 @@ public class TransferableSong implements Transferable {
 			return null;		
 		return this;
 	}
+	
+	@Override
+	public String toString() {
+		return ARTIST + " - " + SONG;
+	}
 }

src/main/java/be/witmoca/BEATs/ui/playlistpanel/PlaylistTableModel.java
@@ -183,11 +183,6 @@ public class PlaylistTableModel extends AbstractTableModel implements DataChange
 			default: return null;
 			}
 		}
-
-		@Override
-		public String toString() {
-			return ARTIST + " - " + SONG + (COMMENT.isEmpty() ? "" : " (" +  COMMENT + ")");
-		}
 		
 		public int getROWID() {
 			return ROWID;